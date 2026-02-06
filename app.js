a46415963a0eca97f02e7826f3959573c1a3fb05
Aryan Shinde <aryanpshinde2006@gmail.com>
2026-02-06 18:51:18 +0530

    refactor: optimize code using objects and loops

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
 
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
 
 const winningScoreSelect = document.querySelector('#playTo')
-
 const resetButton = document.querySelector('#reset');
-
-let p1Score = 0;
-let p2Score = 0;
-
 let winningScore = 3;
 let isGameOver = false;
 
-p1Button.addEventListener('click', () => {
+function updateScore(player, opponent) {
     if (!isGameOver) {
-        p1Score += 1;
-        if (p1Score === winningScore) {
+        player.score += 1;
+        if (player.score === winningScore) {
             isGameOver = true;
-            p1Display.classList.add('has-text-success');
-            p2Display.classList.add('has-text-danger');
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
         }
-        p1Display.innerText = p1Score
+        player.display.innerText = player.score
     }
+}
+
+p1.button.addEventListener('click', () => {
+    updateScore(p1, p2)
 })
 
-p2Button.addEventListener('click', () => {
-    if (!isGameOver) {
-        p2Score += 1;
-        if (p2Score === winningScore) {
-            isGameOver = true;
-            p2Display.classList.add('has-text-success');
-            p1Display.classList.add('has-text-danger');
-        }
-        p2Display.innerText = p2Score
-    }
+p2.button.addEventListener('click', () => {
+   updateScore(p2, p1)
 })
 
 winningScoreSelect.addEventListener('change', function () {
@@ -47,10 +47,10 @@ resetButton.addEventListener('click', reset)
 
 function reset() {
     isGameOver = false
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-    p1Display.classList.remove('has-text-success', 'has-text-danger')
-    p2Display.classList.remove('has-text-success', 'has-text-danger')
+    for(let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
 }
